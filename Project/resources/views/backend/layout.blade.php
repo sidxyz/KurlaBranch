8f3147d99c6ffcf7ab6939d75cbb589e1529f6ca
Faisal khan <faisalpathan31@gmail.com>
2016-10-12 14:34:42 +0530

    Faisal -Another One Down

--- Project/resources/views/backend/layout.blade.php
+++ Project/resources/views/backend/layout.blade.php
@@ -1,13 +1,14 @@
-<html>
+
+1<html>
 
 		<head>	
 			<meta charset="utf-8">
 		    <meta name="viewport" content="width=device-width, initial-scale=1">
-		    <link rel="stylesheet" type="text/css" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+		    <link rel="stylesheet" type="text/css" href="{{ asset('http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css') }}">
 		    <!-- Include Date Range Picker -->
 
-		    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css">
-		    <link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css">
+		    <link rel="stylesheet" type="text/css" href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css') }}">
+		    <link rel="stylesheet" href="{{ asset('http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css') }}">
 
 		    <link rel="stylesheet" type="text/css" href="{{ asset('css\modal.css') }}">
 		    <link rel="stylesheet" type="text/css" href="{{ asset('css\main.css') }}" >
